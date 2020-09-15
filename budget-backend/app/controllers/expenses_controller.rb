fbb95a38555f864d71299a4532d609bd1fce90e1
amkera <60901491+amkera@users.noreply.github.com>
2020-09-14 18:52:24 -0700

    adding new user functionality

--- budget-backend/app/controllers/expenses_controller.rb
+++ budget-backend/app/controllers/expenses_controller.rb
@@ -10,25 +10,25 @@ class ExpensesController < ApplicationController
     render json: expense
   end
 
-  def create
-    user = User.find(params[:user_id])
-    expense = user.expenses.build({
-      name: expense.name,
-      amount: expense.amount
-    })
-    expense.save
-    render json: expense 
-    # if expense.save
-    #   render json: expense
-    # else
-    #   alert: {message: "something went wrong"}
-    # end
-  end
-
-  def destroy
-    expense = Expense.find(params[:id])
-    expense.destroy
-  end
+  # def create
+  #   user = User.find(params[:user_id])
+  #   expense = user.expenses.build({
+  #     name: expense.name,
+  #     amount: expense.amount
+  #   })
+  #   expense.save
+  #   render json: expense
+  #   # if expense.save
+  #   #   render json: expense
+  #   # else
+  #   #   alert: {message: "something went wrong"}
+  #   # end
+  # end
+  #
+  # def destroy
+  #   expense = Expense.find(params[:id])
+  #   expense.destroy
+  # end
 
 
 end
--- budget-frontend/src/index.js
+++ budget-frontend/src/index.js
@@ -2,19 +2,35 @@ const BASE_URL = "http://localhost:3000"
 const USERS_URL = `${BASE_URL}/users`
 const EXPENSES_URL = `${BASE_URL}/expenses`
 const main = document.querySelector("main");
-
+const addUserForm = document.querySelector(".container");
 
 document.addEventListener("DOMContentLoaded", () => loadUsers());
-//anonymous function that invokes loadTrainers
 
 const loadUsers = () => {
-  // same as saying const loadUsers() = function {}
   fetch(USERS_URL)
     .then(res => res.json())
     .then(json => {
       json.forEach(user => renderUser(user))
     })
 }
+
+addUserForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  //get the inputs from the form
+  const userName = e.target.name.value;
+  fetch(USERS_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({name: userName})
+  })
+  .then(res => res.json())
+  .then(newUser => console.log(newUser));
+});
+
+
 
 
 const renderUser = (userHash) => {
@@ -41,6 +57,10 @@ const renderUser = (userHash) => {
   main.appendChild(div);
   userHash.expenses.forEach(expense => renderExpense(expense))
 }
+
+
+
+
 //FOR EACH EXPENSE, there should be a 'delete expense button'
 const renderExpense = (expense) => {
   const ul = document.querySelector(`div[data-id="${expense.user_id}"]`);
