16eb86619f75bd8ed98e49b7ef11d12bb5ee7cc8
amkera <60901491+amkera@users.noreply.github.com>
2020-09-16 19:38:16 -0700

    asked for help and now have dynamic form

--- budget-frontend/src/index.js
+++ budget-frontend/src/index.js
@@ -6,8 +6,6 @@ const addUserForm = document.querySelector(".container-1");
 
 document.addEventListener("DOMContentLoaded", () => loadUsers());
 
-
-
 const loadUsers = () => {
   fetch(USERS_URL)
     .then(res => res.json())
@@ -46,10 +44,10 @@ const renderUser = (userHash) => {
   p.innerHTML = userHash.name;
   button.setAttribute("data-user-id", userHash.id);
   button.innerHTML = "Create Expense"; //<button data-user-id="1">Add Expense</button>
-  //button.addEventListener("click", renderNewExpenseForm);
-  button.addEventListener("click", createExpense{
 
-  }
+  button.addEventListener("click", renderNewExpenseForm);
+  //button.addEventListener("click", createExpense{
+
 
   div.appendChild(p);
   div.appendChild(button);
@@ -59,7 +57,39 @@ const renderUser = (userHash) => {
   userHash.expenses.forEach(expense => renderExpense(expense))
 }
 
+const renderNewExpenseForm = (event) => {
+  const expenseForm = document.createElement('form')
+  expenseForm.setAttribute('id', 'expenseForm')
 
+  const label1 = document.createElement('label')
+  label1.innerText = 'Add Expense Name'
+  const input1 = document.createElement('input')
+  input1.name = 'name'
+  //input.value = ''
+
+  const label2 = document.createElement('label')
+  label2.innerText = 'Add Expense Amount'
+  const input2 = document.createElement('input')
+  input2.type = 'integer'
+
+  const formButton = document.createElement('input')
+  formButton.type = 'submit'
+  formButton.value = 'Add Expense'
+  formButton.setAttribute('data-user_id', event.target.dataset.userId)
+
+
+  formButton.setAttribute('name', 'expenseSubmitButton')
+  expenseForm.append(label1)
+  expenseForm.append(input1)
+
+  expenseForm.append(label2)
+  expenseForm.append(input2)
+
+  expenseForm.append(formButton)
+  event.target.after(expenseForm);
+
+  //VERY IMPORTANT FROM Z: expenseForm.addEventListener("submit"...)
+}
 
 
 //FOR EACH EXPENSE, there should be a 'delete expense button'
