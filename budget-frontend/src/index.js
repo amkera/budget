118a1b33bf21a7fe74fed5d6e911ebe4d91cb95b
amkera <60901491+amkera@users.noreply.github.com>
2020-09-16 18:45:49 -0700

    having trouble with event listener

--- budget-frontend/src/index.js
+++ budget-frontend/src/index.js
@@ -2,9 +2,11 @@ const BASE_URL = "http://localhost:3000"
 const USERS_URL = `${BASE_URL}/users`
 const EXPENSES_URL = `${BASE_URL}/expenses`
 const main = document.querySelector("main");
-const addUserForm = document.querySelector(".container");
+const addUserForm = document.querySelector(".container-1");
 
 document.addEventListener("DOMContentLoaded", () => loadUsers());
+
+
 
 const loadUsers = () => {
   fetch(USERS_URL)
@@ -16,7 +18,6 @@ const loadUsers = () => {
 
 addUserForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  //get the inputs from the form
   const userName = e.target.name.value;
   fetch(USERS_URL, {
     method: "POST",
@@ -41,27 +42,24 @@ const renderUser = (userHash) => {
 
   div.setAttribute("class", "card"); //class="card"
   div.setAttribute("data-id", userHash.id); //data-id="1", id of the user
+
   p.innerHTML = userHash.name;
   button.setAttribute("data-user-id", userHash.id);
   button.innerHTML = "Create Expense"; //<button data-user-id="1">Add Expense</button>
-  button.addEventListener("click", createExpense); //createExpense here corresponds to the #create method in the controller
+  //button.addEventListener("click", renderNewExpenseForm);
+  button.addEventListener("click", createExpense{
+
+  }
+
   div.appendChild(p);
   div.appendChild(button);
   div.appendChild(ul);//This is what puts these elements on page
   main.appendChild(div);
+
   userHash.expenses.forEach(expense => renderExpense(expense))
 }
 
 
-const createExpense = (event) => {
-  event.preventDefault();
-  const expense = {
-    name: ,
-    amount:
-  }
-  // const expenseForm = document.createElement("FORM");
-  // expenseForm.setAttribute("expense-id", expense.id);
-}
 
 
 //FOR EACH EXPENSE, there should be a 'delete expense button'
