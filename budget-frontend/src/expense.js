ec525929d58e1e794e805abfd5b9c0a4be7519ec
amkera <60901491+amkera@users.noreply.github.com>
2020-09-18 14:37:37 -0700

    separating js code into appropriate gfiles

---
+++ budget-frontend/src/expense.js
@@ -0,0 +1,93 @@
+
+
+
+const renderNewExpenseForm = (event) => {
+  const expenseForm = document.createElement('form')
+  expenseForm.setAttribute('id', 'expenseForm')
+  expenseForm.setAttribute('class', event.target.dataset.userId)
+
+  const label1 = document.createElement('label')
+  label1.innerText = 'Add Expense Name'
+  const input1 = document.createElement('input')
+  input1.name = 'name'
+  //input.value = ''
+
+  const label2 = document.createElement('label')
+  label2.innerText = 'Add Expense Amount'
+  const input2 = document.createElement('input')
+  input2.type = 'number'
+  input2.min = 0
+  input2.name = 'amount'
+
+  const formButton = document.createElement('input')
+  formButton.type = 'submit'
+  formButton.value = 'Add Expense'
+  //formButton.setAttribute('data-user_id', event.target.dataset.userId)
+  formButton.setAttribute('id', event.target.dataset.userId)
+
+  const removeFormButton = document.createElement('button');
+  //removeFormButton.innerText = "Back"
+
+  formButton.setAttribute('name', 'expenseSubmitButton')
+  expenseForm.append(label1)
+  expenseForm.append(input1)
+
+  expenseForm.append(label2)
+  expenseForm.append(input2)
+
+  expenseForm.append(formButton)
+  //expenseForm.append(removeFormButton);
+
+  event.target.after(expenseForm);
+  expenseForm.addEventListener("submit", createExpense)
+  //VERY IMPORTANT FROM Z: expenseForm.addEventListener("submit"...)
+}
+
+function createExpense(e) {
+  e.preventDefault();
+  let expenseName = e.target.name.value;
+  let expenseAmount =  e.target.amount.value;
+  let users_id = e.target.className;
+  fetch(EXPENSES_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({name: expenseName, amount: expenseAmount, user_id: users_id})
+  }).then(resp => {
+    return resp.json()
+  }).then(expense => {
+    renderExpense(expense)
+  })
+
+}
+
+const renderExpense = (expense) => {
+  const ul = document.querySelector(`div[data-id="${expense.user_id}"]`);
+  const li = document.createElement("li")
+  const button = document.createElement("button")
+  li.innerHTML = `${expense.name}: $${expense.amount} `
+  button.setAttribute("button", "delete")
+  button.setAttribute("data-expense-id", expense.id)
+  button.addEventListener("click", deleteExpense)
+  //this is how JS knows what is being deleted, because the expense id is being set on the button
+  button.innerHTML = "Delete"
+  li.appendChild(button)
+  ul.appendChild(li);
+}
+
+function deleteExpense(e) {
+  e.preventDefault();
+//remove it from db
+  const configObj = {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    }
+  }
+  fetch(`${EXPENSES_URL}/${e.target.dataset["expenseId"]}`, configObj)
+  //remove it from the page
+  e.target.parentElement.remove();
+}
---
+++ budget-frontend/src/user.js
@@ -0,0 +1,53 @@
+document.addEventListener("DOMContentLoaded", () => loadUsers());
+
+const loadUsers = () => {
+  fetch(USERS_URL)
+    .then(res => res.json())
+    .then(json => {
+      json.forEach(user => renderUser(user))
+    })
+}
+
+addUserForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const userName = e.target.name.value;
+  fetch(USERS_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({name: userName})
+  })
+    .then(res => res.json())
+    .then(newUser => renderUser(newUser))
+    main.append(newUser);
+});
+
+
+const renderUser = (userHash) => {
+  //trainerHash is the argument, and this hash is for one trainer only
+  const div = document.createElement("div");
+  const p = document.createElement("p");
+  const button = document.createElement("button");
+  const ul = document.createElement("p");
+
+  div.setAttribute("class", "card"); //class="card"
+  div.setAttribute("data-id", userHash.id); //data-id="1", id of the user
+
+  p.innerHTML = userHash.name;
+  button.setAttribute("data-user-id", userHash.id);
+  button.innerHTML = "Create Expense"; //<button data-user-id="1">Add Expense</button>
+
+  button.addEventListener("click", renderNewExpenseForm);
+  button.addEventListener("click", (event) => {
+    event.target.hidden = true;
+  });
+
+  div.appendChild(p);
+  div.appendChild(button);
+  div.appendChild(ul);
+  main.appendChild(div);
+
+  userHash.expenses.forEach(expense => renderExpense(expense))
+}
